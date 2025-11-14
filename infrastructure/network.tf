0b2426a48607fa89156940de6a19b91c596f353e
igorsilva-dev <dev@igorsilva.dev>
2025-11-14 20:51:10 +0100

    test deployment workflow

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source               = "git::https://github.com/igorsilva-dev/tf-modules.git//civo/network?ref=v2025.11.12.02"
   network_label        = "main_network"
-  firewall_name        = "main_firewall"
+  firewall_name        = "main_firewal"
   create_default_rules = true
 }
