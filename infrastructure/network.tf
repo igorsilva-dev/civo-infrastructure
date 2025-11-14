cb1f948953e0403ebdea9a4097ba6ac461a8e847
igorsilva-dev <dev@igorsilva.dev>
2025-11-14 15:31:00 +0100

    fix linting

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -1,6 +1,6 @@
 module "network" {
-  source       = "git::https://github.com/igorsilva-dev/tf-modules.git//civo/network?ref=v2025.11.12.02"
-  network_label = "main_network"
-  firewall_name  = "main_firewall"
+  source               = "git::https://github.com/igorsilva-dev/tf-modules.git//civo/network?ref=v2025.11.12.02"
+  network_label        = "main_network"
+  firewall_name        = "main_firewall"
   create_default_rules = true
 }
